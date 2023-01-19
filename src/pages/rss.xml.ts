358b7ff31db8cee2f2e67c9855176fc57d1dc442
Pablo Ferreiro <pferreiromero@gmail.com>
2023-01-20 00:54:10 +0100

    hotfix

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,7 +1,7 @@
 import rss from "@astrojs/rss";
 
 export const get = () => {
-  rss({
+  return rss({
     title: "Pablo Ferreiro's blog",
     description: "My personal dev blog",
     site: import.meta.env.SITE,
