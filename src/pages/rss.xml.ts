388036df803bf0394cb7a060760e6330fc391b91
Pablo Ferreiro <pferreiromero@gmail.com>
2023-01-20 00:52:28 +0100

    small fixes

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,9 +1,10 @@
 import rss from "@astrojs/rss";
 
-export const get = () =>
+export const get = () => {
   rss({
     title: "Pablo Ferreiro's blog",
     description: "My personal dev blog",
     site: import.meta.env.SITE,
     items: import.meta.glob("./blog/*.md"),
   });
+}
